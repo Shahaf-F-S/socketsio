522485ceafa37e4f216bc4777b4c8ca9490bbf63
Shahaf Frank-Shapir <67577414+Shahaf-F-S@users.noreply.github.com>
2023-09-09 10:30:15 +0300

    Project Update

--- client.py
+++ client.py
@@ -1,16 +1,22 @@
 # client.py
 
-import socket
-
-from sockets_communication.client import SocketClient
+from socketsio import Client, TCP
 
 def main() -> None:
     """Runs the test to test the program."""
 
-    client = SocketClient()
-    client.connect(socket.socket(), host="127.0.0.1", port=5000)
-    client.send_message_to_server("hello".encode())
-    client.send_message_to_server("hello".encode())
+    host = "127.0.0.1"
+    port = 5555
+
+    protocol = TCP()
+
+    client = Client(protocol)
+    client.connect((host, port))
+
+    for _ in range(2):
+        client.send(("hello world" * 1).encode())
+        print(client.receive())
+    # end for
 # end main
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,15 +1,49 @@
 # server.py
 
 import socket
+from typing import Tuple
 
-from sockets_communication.server import SocketServer
+from socketsio import Server, TCP, BaseProtocol
+
+Connection = socket.socket
+Address = Tuple[str, int]
+
+def respond(
+        connection: Connection,
+        address: Address,
+        protocol: BaseProtocol
+) -> None:
+    """
+    Sets or updates clients data in the clients' container .
+
+    :param protocol: The protocol to use for sockets communication.
+    :param connection: The socket object of the server.
+    :param address: The address of the connection.
+    """
+
+    while True:
+        received = protocol.receive(connection=connection, address=address)
+        if not received:
+            continue
+        # end if
+
+        sent = f"server received from {address}: ".encode() + received
+
+        protocol.send(connection=connection, data=sent, address=address)
+    # end while
+# end action
 
 def main() -> None:
     """Runs the test to test the program."""
 
-    server = SocketServer()
-    server.connect(socket.socket(), host="127.0.0.1", port=5000)
-    server.listen()
+    host = "127.0.0.1"
+    port = 5555
+
+    protocol = TCP()
+
+    server = Server(protocol)
+    server.bind((host, port))
+    server.serve(action=respond, block=False)
 # end main
 
 if __name__ == "__main__":
