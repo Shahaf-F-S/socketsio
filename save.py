7d58d94675dda3dccd37b9b32ab8cbc8fd933bb8
Shahaf Frank-Shapir <shahaffrs@gmail.com>
2023-11-06 15:09:56 +0200

    Project Update

--- save.py
+++ save.py
@@ -1,13 +1,12 @@
 # save.py
 
 import os
-from typing import Optional
 
 from base import run_silent_command, suppress
 from specs import project_specs
 from document import generate_html
 
-def main(project: str, silence: Optional[bool] = True) -> None:
+def main(project: str, silence: bool = True) -> None:
     """
     Runs the function to save thew project.
 
